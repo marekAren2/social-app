ae3522cffce1213b78f4554952c2d128675b37e9
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-11-26 23:55:27 +0100

    8:07Cannot read propert of undef (reading 'value')

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,10 +2,17 @@ import { useState } from "react";
 import "./Login.css"
 
 const Login = () => {
+
+    // 5:14 
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+
+  const handleInputChange = (e) => {
+    //7:46 brak .value
+    console.log(e.taget.value);
+  }
 
   // loginMessages, setLoginMess 23:49
   //axios
@@ -18,21 +25,28 @@ const Login = () => {
 
   return (
     <div className="login">
-      <form className="formLogin">
+      <form className="formLogin" 
+    // moja inwencja daje blad
+    //   onChange={handleInputChange()}
+      >
         {/* //{}loginMessages */}
         <input
           type="text"
           className="username"
           placeholder="User name"
-          value={formData.username}
+        //6:13 wlasciwosc username: obiektu formData ustawiona przez setter w useState
+          //   value={formData.username}
           name="username"
+          onChange={handleInputChange}
         />
         <input
           type="password"
           className="password"
           placeholder="Password"
-          value={formData.password}
+        // 6:30 valu powinno byc odpowiadac stanowi stad
+          //   value={formData.password}
           name="password"
+          onChange={handleInputChange}
         />
         <button type="submit" className="btn btnSubmitLogin">
           {" "}
