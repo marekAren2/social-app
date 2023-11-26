5dde4173a458d7b162e07021f54e9f28db356839
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-11-26 23:18:11 +0100

    ASK:  po paddin 10 10px inputy wyszly poza form

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,33 +1,47 @@
 import { useState } from "react";
+import "./Login.css"
 
 const Login = () => {
-    
-    const [formData, setFormData] = useState ({
-        
-        username: "",
-        password: ""
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-    })
-    
-    // loginMessages, setLoginMess 23:49
-    //axios
- //error if(Array.isArray(res.data.username) tst na tablicowosc
- // setLoginMes()
- //26:56 else
- //props.setitem bledy logowania
+  // loginMessages, setLoginMess 23:49
+  //axios
+  //error if(Array.isArray(res.data.username) tst na tablicowosc
+  // setLoginMes()
+  //26:56 else
+  //props.setitem bledy logowania
 
-//33:29 axios authorization Bearer + (user ?.jwt_token: "" )
+  //33:29 axios authorization Bearer + (user ?.jwt_token: "" )
 
-    return (
+  return (
     <div className="login">
-        <form >
-            {/* //{}loginMessages */}
-            <input type="text" className="login" placeholder="user" value={formData.username}/>
-            <input type="password" className="password" placeholder="password" value={formData.password}/>
-        </form>
+      <form className="formLogin">
+        {/* //{}loginMessages */}
+        <input
+          type="text"
+          className="username"
+          placeholder="User name"
+          value={formData.username}
+          name="username"
+        />
+        <input
+          type="password"
+          className="password"
+          placeholder="Password"
+          value={formData.password}
+          name="password"
+        />
+        <button type="submit" className="btn btnSubmitLogin">
+          {" "}
+          Login
+        </button>
+      </form>
     </div>
-    )
-}
+  );
+};
 
 export default Login;
 // przed 12: myk z funkcja dla 2 poll [name]
