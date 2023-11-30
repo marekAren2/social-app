e731c4ae56070e986daec84cf5563b81c053f6db
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-11-30 15:37:05 +0100

    11:05 all mod naloz n/sieb chcemy ze 1 wysw  occh?

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import "./Post.css"
 import { useState } from "react";
 
@@ -11,7 +12,19 @@ const Post = (props) => {
     let postObject= props.post;
 
     const [likesCount, setLikesCount] = useState (postObject.likes.length)
+    // 11:05 wiele modeli nalozonych na siebie chcemy zeby tylko jesen sie wyswietlal
+    //o co chodzi?
 
+    // 5:35 funkcja deletePost(id post)
+    const deletePost = (id) => {
+     axios.post('http://akademia108.pl/api/social-app/post/delete')
+     .then((res)=>{
+        //co tu? <7:57 teras confirm delete?
+     }) 
+     .catch((error)=>{
+        console.warn(error);
+     })  
+    }
     // console.log('post z postObject ' , postObject.id);
     // let props.post=props;
     // console.log("🚀 ~ file: props.post.js:4 ~ props.post ~ props.post:", props.post)
@@ -33,7 +46,8 @@ const Post = (props) => {
                 <div className="postContent">{postObject.content}</div>
                 <div className="likes">
                     {/* // v29 01:27 czemu button do likes */}
-                    {props.user.username === props.post.user.username && <button className="btn">Delete</button> }
+                    {/* //3:50 props.user?.username wtedy moze byc null */}
+                    {props.user?.username === props.post.user.username && <button className="btn">Delete</button> }
                     {/* // 2:12 button wyswietla dla wszystkich chcemy przy swoich */}
                     {/* uciekamy sie do ciekawego warunku renderowania przycisku      */}
                     {likesCount}</div> 
@@ -46,6 +60,13 @@ const Post = (props) => {
             {/* style={color: 'red';} error bo ;*/}
             {/* <p>{postObject.content}</p> */}
             {/* <p className="likeFooter">{postObject.likes.length}</p> */}
+            {/* // 7:57 */}
+            <div className="deleteConfirmation">
+                <h3>Are you sure you want to delete post?</h3>
+                {/* // 8:56 stylowanie zrob */}
+                <button className="btn yes">Yes</button>
+                <button className="btn no">No</button>
+            </div>
         </div>
     );
 }
