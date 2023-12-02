eab96ffe23de2f324b70687846e1670d9a708ae5
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-12-02 20:56:33 +0100

    post jest wezszy od follow i addPost o jakies 5px n/moge find

--- src/components/FollowRecommendations.js
+++ src/components/FollowRecommendations.js
@@ -1,6 +1,7 @@
 // 00:05 opis
 // 3:05 
-
+// 12:41
+import "./FollowRecommendations.css"
 import axios from "axios";
 import { useEffect, useState } from "react"
 
@@ -16,16 +17,32 @@ export const FollowRecommendations = (props) => {
             .catch((error)=>{
                 console.warn(error);
             })
-    };
+    }; 
 
-    // 7:44 kiedy chcemy , po zaladowaniu komponentuwywołac use Effect z pomoca
+    // 7:44 kiedy chcemy , po załadowaniu komponentu wywołac use Effect z pomoca
     useEffect(()=>{
         // 08:46 wtedy nowa lista rekomendacji
         getRecommendations();
-      // 08:24 dlatego w propsach przekazano posts liste postow, 
+    //   08:24 dlatego w propsach przekazano posts liste postow, 
     //   bo jak cos sie zmieni w postach bo sa podane w tablicy zaleznosci, to bedzie uruchomione  getRecommendations... 
     },[props.posts])
+    
+    console.log(recommendations );
     return (
-        <div className="followRecommendations">follows</div>
+        <div className="followRecommendations">{recommendations.map((recommendation)=>{
+            return (
+                // 10:29 div with key from recommendation
+                <div className="followRecommendation" key={recommendation.id}>
+                    <img src={recommendation.avatar_url} alt={recommendation.username} />
+                    {/* // 11:18 w h3 user */}
+                    <h3>{recommendation.username}</h3>
+                    <button className="btn">Follow</button>
+                    {/* // 12:26 stylujemy */}
+                </div>
+            )
+
+            })}
+        </div>
+        
     )
 }
